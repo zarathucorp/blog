71abdc36adb220d0f173c492e8340ec8398a6659
jaewoong heo <koolerjaebee@gmail.com>
2025-08-13 15:10:30 +0900

    fix: remove _freeze and shinylive install issue fix

---
+++ _install_packages.R
@@ -0,0 +1,8 @@
+# _install_packages.R
+
+# install shinylive package
+# from r-universe
+install.packages("shinylive", repos = c("https://posit-dev.r-universe.dev", getOption("repos")))
+
+# install other necessary R packages
+# e.g., install.packages(c("tidyverse", "ggplot2", "shiny"))
